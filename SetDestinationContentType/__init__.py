bab053d6eb27cb2d2ac6741eeb9c802b3a7c5e24
Glenn Musa <glennmusa@users.noreply.github.com>
2020-12-10 12:18:25 -0500

    Add a base case

--- SetDestinationContentType/__init__.py
+++ SetDestinationContentType/__init__.py
@@ -9,38 +9,44 @@ def main(destblob: func.InputStream):
                  f"Name: {destblob.name}\n"
                  f"Blob Size: {destblob.length} bytes")
 
+    blob_name = os.path.basename(destblob.name)
+    default_default_blob_byte_type = "application/octet-stream"
+
     source_connection_string = os.environ["source_connection_string"]
     source_container_name = os.environ["source_container_name"]
-    source_blob_name = os.path.basename(destblob.name)
 
     destination_connection_string = os.environ["destination_connection_string"]
     destination_container_name = os.environ["destination_container_name"]
 
-    logging.info(
-        f"Instantiate client for {source_blob_name} in {source_container_name}")
-
-    source_client = BlobClient.from_connection_string(
-        source_connection_string,
-        source_container_name,
-        source_blob_name
-    )
-
-    properties = source_client.get_blob_properties()
-    name = properties.name
-    content_type = properties.content_settings.content_type
-
-    logging.info(
-        f"Instantiate client for {name} in {destination_container_name}")
-
     destination_client = BlobClient.from_connection_string(
         destination_connection_string,
         destination_container_name,
-        name)
+        blob_name)
 
-    logging.info(f"Set headers for {name} in {destination_container_name}")
+    current_content_type = destination_client.get_blob_properties(
+    ).content_settings.content_type
 
-    destination_client.set_http_headers(
-        content_settings=ContentSettings(content_type=content_type)
-    )
+    if(current_content_type == default_blob_byte_type):
+        logging.info(
+            f"Blob has content-type '{default_blob_byte_type}'. Sourcing the content-type from {source_container_name}.")
 
-    logging.info(f"done")
+        source_client = BlobClient.from_connection_string(
+            source_connection_string,
+            source_container_name,
+            blob_name
+        )
+
+        source_properties = source_client.get_blob_properties()
+        source_content_type = source_properties.content_settings.content_type
+
+        logging.info(
+            f"Setting content-type for {blob_name} in {destination_container_name} to {source_content_type}")
+
+        destination_client.set_http_headers(
+            content_settings=ContentSettings(content_type=source_content_type)
+        )
+
+        logging.info(f"Done setting content-type for {blob_name}")
+    else:
+        logging.info(
+            f"Blob already has a content-type '{current_content_type}'. Exiting.")
